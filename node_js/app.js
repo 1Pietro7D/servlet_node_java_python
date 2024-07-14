47436dda23576d1f7818041eb3d366eb50286efb
PietroDanieli <1pie7d@gmail.com>
2024-07-14 02:24:52 +0200

    add chatMessages [] Array and can get it in /chat

--- node_js/app.js
+++ node_js/app.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 
 const app = express();
 const port = 5001;
+const chatMessages = [];
 
 // Middleware per gestire il parsing del corpo delle richieste
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,7 +34,7 @@ app.post('/send', async (req, res) => {
     // URL di destinazione per i diversi target
     const urls = [
         'http://127.0.0.1:5002/python',  // URL del server Python
-        // 'http://localhost:5002/python', DX non funziona
+        // 'http://localhost:5002/python', DX non funziona mentre in Java funziona
         'http://localhost:5003/java'     // URL del server Java
     ];
 
@@ -41,13 +42,16 @@ app.post('/send', async (req, res) => {
         if (target === 'all') {
             // Invio del messaggio a tutti i server
             await Promise.all(urls.map(url => axios.post(url, { from: "node", message: message })));
+            chatMessages.push(message)
         } else {
             const index = targetToIndex(target);
             if (index !== null) {
                 // Invio del messaggio al server specificato
                 await axios.post(urls[index], { from: "node", message: message });
+                chatMessages.push(message)
             } else {
-                res.status(200).send('Sei una brutta persona');
+                res.send('Sei una brutta persona');
+                return
             }
         }
         res.send('Messaggio inviato');
@@ -61,7 +65,13 @@ app.post('/send', async (req, res) => {
 app.post('/node', (req, res) => {
     const data = req.body;
     console.log(`Messaggio ricevuto: ${data.message} from ${data.from}`);
+    chatMessages.push(data)
     res.json({ status: 'Messaggio ricevuto' });
+});
+
+// Endpoint per ottenere tutti i messaggi della chat
+app.get('/chat', (req, res) => {
+    res.json(chatMessages);
 });
 
 // Avvia il server
