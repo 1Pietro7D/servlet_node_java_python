7bbd061d3f01b21a729f9fcbabae2816c49e9f10
PietroDanieli <1pie7d@gmail.com>
2024-07-16 11:33:25 +0200

    add for WebController sessionsSocket connection action

--- java/demo/src/main/java/com/example/demo/controllers/WebController.java
+++ java/demo/src/main/java/com/example/demo/controllers/WebController.java
@@ -2,24 +2,27 @@ package com.example.demo.controllers;
 
 
 import java.util.Map;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.web.bind.annotation.*;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.client.RestTemplate;
-
-import com.example.demo.models.Message;
-
 import org.springframework.http.ResponseEntity;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.socket.TextMessage;
+import com.example.demo.handler.SessionsSocket;
+import com.example.demo.models.Message;
 
 @Controller
 public class WebController {
-
+    //@Autowired
+    //private SessionsSocket sessionsSocket;
+    
+    private SessionsSocket sessionsSocket = SessionsSocket.getInstance();
     private List<Message> messages = new ArrayList<>();
 
     // Definisce la route per la pagina principale
@@ -31,7 +34,7 @@ public class WebController {
 
     // Definisce la route per inviare messaggi
     @PostMapping("/send")
-    public String sendMessage(@RequestParam String message, @RequestParam String target /*, Model model*/) {
+    public String sendMessage(@RequestParam String message, @RequestParam String target /*, Model model*/) throws IOException {
         // Definisce gli URL di destinazione per i diversi target
         String[] urls = {
             "http://localhost:5001/node",
@@ -54,30 +57,38 @@ public class WebController {
         } else if (target.equals("python")) {
             restTemplate.postForEntity(urls[1], entity, String.class);
         }
+        
+        Message newMessage = new Message("Java_Server", message);
 
         // Aggiunge il messaggio inviato alla lista dei messaggi
-        messages.add(new Message("Java_Server", message));
-
-        // // Aggiunge un messaggio di stato al modello
-        // model.addAttribute("status", "Messaggio inviato");
-        // model.addAttribute("messages", messages);
-
-        return "redirect:/";
+        messages.add(newMessage);
+        
+        sessionsSocket.sendMessageToAllSessions(new TextMessage(newMessage.toString()));
+        // Ottenere tutte le sessioni WebSocket
+        // List<WebSocketSession> allSessions = sessionsSocket.getSessions();
+        // for (WebSocketSession session : sessionsSocket.getSessions()) {
+        //     // System.out.println(session.getId());
+        //     //TextMessage newTextMessage = new TextMessage("nuovo testo");
+        //     session.sendMessage(new TextMessage(newMessage.toString()));
+        // }
+        
+            return "redirect:/";
     }
 
-    // Definisce la route per ricevere messaggi
+    // define the route to receive the message
     @PostMapping("/java")
     @ResponseBody
-    public ResponseEntity<String> receiveMessage(@RequestBody Map<String, String> payload) {
+    public ResponseEntity<String> receiveMessage(@RequestBody Map<String, String> payload) throws IOException {
         String from = payload.get("from");
         String message = payload.get("message");
+        System.out.println("payload: " + payload);
+        // System.out.println("From: " + from);
+        // System.out.println("Message: " + message);
 
-        System.out.println("From: " + from);
-        System.out.println("Message: " + message);
-
-        // Aggiunge il messaggio ricevuto alla lista dei messaggi
-        messages.add(new Message(from, message));
-
+        // Add received message to the list
+        Message newMessage = new Message(from, message);
+        messages.add(newMessage);
+        sessionsSocket.sendMessageToAllSessions(new TextMessage(newMessage.toString()));
         return ResponseEntity.ok("Messaggio ricevuto");
     }
 }
